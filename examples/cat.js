3dc6cbfff3618b1a0e7d90a6c6ab05394f5c25aa
Michiel de Jong <michiel@ripple.com>
2017-10-04 12:20:10 +0200

    feat: add Cat

--- examples/cat.js
+++ examples/cat.js
@@ -1,17 +1,6 @@
-# btp-toolbox
-Tools for building and debugging with the Bilateral Transport Protocol (BTP)
-
-# <img src="./assets/cat.svg" width="25px"> Cat
-
-BtpCat is used strictly for logging.
-It can be used to display BTP packets, and the ILP packets inside them,
-in a nice human-readable way. It's a single function that takes a JavaScript object, and
-outputs a String so that `eval(BtpCat(obj)) === obj`. Example usage:
-
-```js
 const BtpPacket = require('btp-packet')
 const IlpPacket = require('ilp-packet')
-const BtpCat = require('./src/cat')
+const BtpCat = require('../src/cat')
 const chalk = require('chalk')
 
 console.log(chalk.bold.green('alice sends:'), chalk.green(BtpCat({
@@ -40,7 +29,7 @@ console.log(chalk.bold.green('alice sends:'), chalk.green(BtpCat({
   }
 }, BtpPacket.BTP_VERSION_1)))
 
-console.log(chalk.bold.red('alice receives:', BtpCat({
+console.log(chalk.bold.red('alice receives:'), chalk.red(BtpCat({
   type: BtpPacket.TYPE_RESPONSE,
   requestId: 4,
   data: {
@@ -68,8 +57,3 @@ console.log(chalk.bold.red('alice receives:', BtpCat({
     ]
   }
 }, BtpPacket.BTP_VERSION_1)))
-```
-
-Example output:
-
-<img src="./assets/screenshot.png" width="500px">
